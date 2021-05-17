ec3dd170050cc8850a35aa2327412f1f5f6c125e
ferranSanchezUB <31955699+ferranSanchezUB@users.noreply.github.com>
2021-05-17 22:45:45 +0200

    Detoxis competition + other enchaments
    - Detoxis dataset
    - SBERT (only bert)
    - Hyperparameter search model
    - Recalculated tfidf
    - Smote Sbert
    - Stratified Kfold on pytorch models
    - Choose best models for detoxis competition
    - Tested bert avarage with two different lr (one for the new pooler and the same for the rest)

--- requirements/install-atalaya.sh
+++ requirements/install-atalaya.sh
@@ -1,5 +1,5 @@
 source ~/anaconda3/etc/profile.d/conda.sh
-conda create --name tf-gpu-1.15 --file requirements-conda-atalaya.txt
+conda create -y --name tf-gpu-1.15 --file requirements-conda-atalaya.txt
 conda activate tf-gpu-1.15
 conda install -y -c anaconda notebook
 pip install -r requirements-pip-base.txt
