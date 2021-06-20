e43be163e9e4cd6053247ba5e5f55c10d17a3704
ferranSanchezUB <31955699+ferranSanchezUB@users.noreply.github.com>
2021-06-20 21:17:06 +0200

    Modified installation shells
    - Added channels to the conda installation

--- requirements/install-atalaya.sh
+++ requirements/install-atalaya.sh
@@ -1,5 +1,5 @@
 source ~/anaconda3/etc/profile.d/conda.sh
-conda create -y --name tf-gpu-1.15 --file requirements-conda-atalaya.txt
+conda create -y --name tf-gpu-1.15 --file requirements-conda-atalaya.txt --channel conda-forge  --channel pytorch
 conda activate tf-gpu-1.15
-conda install -y -c anaconda notebook
+conda install -y -c anaconda notebook --channel conda-forge
 pip install -r requirements-pip-base.txt
--- requirements/install-base.sh
+++ requirements/install-base.sh
@@ -1,5 +1,5 @@
 source ~/anaconda3/etc/profile.d/conda.sh
-conda create -y --name tf-gpu --file requirements-conda-base.txt
+conda create -y --name tf-gpu --file requirements-conda-base.txt --channel conda-forge  --channel pytorch
 conda activate tf-gpu
-conda install -y -c anaconda notebook
+conda install -y -c anaconda notebook --channel conda-forge
 pip install -r requirements-pip-base.txt
